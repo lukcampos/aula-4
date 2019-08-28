81c7c5be0cd400db1b13b44aa4aa2dacd255b83d
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-08-27 22:49:48 -0300

    Países dinâmicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 //importando o Angular Forms
 import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
+// importando o Angular HttpCLient
+import {HttpClientModule} from '@angular/common/http'
+
+// npm set strict-ssl false
 
 //forma antiga de exportar
 // import { FormComponent } from './form/form.component';
@@ -14,6 +18,7 @@ import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 //forma nova
 import { FormComponent } from './components/index';
 
+// https://github.com/lukcampos/aula-4
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +28,8 @@ import { FormComponent } from './components/index';
     BrowserModule,
     AppRoutingModule,
     FormsModule,// modulo do angular forms
-    ReactiveFormsModule// modulo do angular forms
+    ReactiveFormsModule, // modulo do angular forms,
+    HttpClientModule // modulo do angular HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms'
+// importando o Angular HttpCLient
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,7 +9,29 @@ import {FormBuilder, FormGroup} from '@angular/forms'
 })
 export class FormComponent implements OnInit {
   mainForm:FormGroup;
-  constructor(private formBuilder:FormBuilder) {
+  generos:Array<Object> = [];// array de generos
+  cores:Array<Object> = []; // array de cores
+  urlPaises:string = "https://restcountries.eu/rest/v2/all"
+  paises:any = [];
+  constructor(
+    private formBuilder:FormBuilder,
+    private http:HttpClient // inport HttpClient
+    ) {
+    //adicionar array de generos
+    this.generos = [
+    {nome:"Masculino", valor:"m"},
+    {nome:"Feminino", valor:"f"},
+    {nome:"Outros", valor:"o"},
+    ]
+
+    //adicionar array de cores
+    this.cores = [
+      {nome:"Vermelho", hex:"#ff2f2f"},
+      {nome:"Amarelo", hex:"#ffd400"},
+      {nome:"azul", hex:"#007eff"},
+      {nome:'Rosa',hex:'#e400ff' },
+      {nome:'Verde',hex:'#008246' }
+    ]
 
     // adicionar os atributos ao formulário
     this.mainForm = this.formBuilder.group({
@@ -21,7 +45,11 @@ export class FormComponent implements OnInit {
     })
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+
+    var paises = await  this.http.get(this.urlPaises).toPromise()
+    this.paises = paises
+
   }
 
 }
